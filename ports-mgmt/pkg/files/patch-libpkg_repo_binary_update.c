2914685ef9bcb34aeaac5c94aa5a6c8a095cdce8
Franco Fichtner <franco@opnsense.org>
2023-11-29 14:16:53 +0100

    ports-mgmt/pkg: v4

--- ports-mgmt/pkg/files/patch-libpkg_repo_binary_update.c
+++ ports-mgmt/pkg/files/patch-libpkg_repo_binary_update.c
@@ -8,55 +8,63 @@
  #include <sys/time.h>
  
  #include <stdio.h>
-@@ -563,12 +564,12 @@ cleanup:
- 		if (pkgdb_transaction_commit_sqlite(sqlite, "REPO") != EPKG_OK)
- 			rc = EPKG_FATAL;
- 	}
--	/* restore the previous db in case of failures */
--	if (rc != EPKG_OK && rc != EPKG_UPTODATE) {
--		unlink(name);
--		rename(path, name);
--	}
- 	if (path != NULL) {
-+		/* restore the previous db in case of failures */
-+		if (rc != EPKG_OK && rc != EPKG_UPTODATE) {
-+			unlink(name);
-+			rename(path, name);
-+		}
- 		unlink(path);
- 		free(path);
- 	}
-@@ -586,13 +587,14 @@ int
+@@ -586,13 +587,15 @@ int
  pkg_repo_binary_update(struct pkg_repo *repo, bool force)
  {
  	char filepath[MAXPATHLEN];
 +	char *lockpath = NULL;
  	const char update_finish_sql[] = ""
  		"DROP TABLE repo_update;";
++	const char *filename;
  	sqlite3 *sqlite;
  
  	struct stat st;
  	time_t t = 0;
 -	int res = EPKG_FATAL;
-+	int ld, res = EPKG_FATAL;
++	int dfd, ld, res = EPKG_FATAL;
  
  	bool got_meta = false;
  
-@@ -603,7 +605,7 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+@@ -601,32 +604,47 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+ 	if (!pkg_repo_enabled(repo))
+ 		return (EPKG_OK);
  
- 	pkg_debug(1, "PkgRepo: verifying update for %s", pkg_repo_name(repo));
+-	pkg_debug(1, "PkgRepo: verifying update for %s", pkg_repo_name(repo));
++	pkg_debug(1, "PkgRepo: verifying update for %s", repo->name);
  
--	/* First of all, try to open and init repo and check whether it is fine */
-+	/* First of all, try to open repo and check whether it is fine */
++	filename = pkg_repo_binary_get_filename(repo->name);
++
+ 	/* First of all, try to open and init repo and check whether it is fine */
  	if (repo->ops->open(repo, R_OK|W_OK) != EPKG_OK) {
- 		pkg_debug(1, "PkgRepo: need forced update of %s", pkg_repo_name(repo));
+-		pkg_debug(1, "PkgRepo: need forced update of %s", pkg_repo_name(repo));
++		pkg_debug(1, "PkgRepo: need forced update of %s", repo->name);
  		t = 0;
-@@ -627,6 +629,18 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+ 		force = true;
+ 		snprintf(filepath, sizeof(filepath), "%s/%s", ctx.dbdir,
+-		    pkg_repo_binary_get_filename(pkg_repo_name(repo)));
++		    filename);
+ 	}
+ 	else {
+ 		repo->ops->close(repo, false);
+-		snprintf(filepath, sizeof(filepath), "%s/%s.meta", ctx.dbdir, pkg_repo_name(repo));
++		snprintf(filepath, sizeof(filepath), "%s/%s.meta", ctx.dbdir, repo->name);
+ 		if (stat(filepath, &st) != -1) {
+ 			t = force ? 0 : st.st_mtime;
+ 			got_meta = true;
+ 		}
+ 
+ 		snprintf(filepath, sizeof(filepath), "%s/%s", ctx.dbdir,
+-			pkg_repo_binary_get_filename(pkg_repo_name(repo)));
++			filename);
+ 		if (got_meta && stat(filepath, &st) != -1) {
+ 			if (!force)
+ 				t = st.st_mtime;
  		}
  	}
  
-+	xasprintf(&lockpath, "%s-lock", filepath);
-+	ld = open(lockpath, O_CREAT|O_TRUNC|O_WRONLY, 00644);
++	xasprintf(&lockpath, "%s-lock", filename);
++	dfd = pkg_get_dbdirfd();
++	ld = openat(dfd, lockpath, O_CREAT|O_TRUNC|O_WRONLY, 00644);
 +	if (flock(ld, LOCK_EX|LOCK_NB) == -1) {
 +		/* lock blocking anyway to let the other end finish */
 +		pkg_emit_notice("Waiting for another process to "
@@ -70,7 +78,7 @@
  	res = pkg_repo_binary_update_proceed(filepath, repo, &t, force);
  	if (res != EPKG_OK && res != EPKG_UPTODATE) {
  		pkg_emit_notice("Unable to update repository %s", repo->name);
-@@ -640,6 +654,16 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+@@ -640,6 +658,16 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
  	}
  
  cleanup:
@@ -80,10 +88,19 @@
 +	}
 +
 +	if (lockpath != NULL) {
-+		unlink(lockpath);
++		unlinkat(dfd, lockpath, 0);
 +		free(lockpath);
 +	}
 +
  	/* Set mtime from http request if possible */
  	if (t != 0 && res == EPKG_OK) {
  		struct timeval ftimes[2] = {
+@@ -655,7 +683,7 @@ cleanup:
+ 
+ 		utimes(filepath, ftimes);
+ 		if (got_meta) {
+-			snprintf(filepath, sizeof(filepath), "%s/%s.meta", ctx.dbdir, pkg_repo_name(repo));
++			snprintf(filepath, sizeof(filepath), "%s/%s.meta", ctx.dbdir, repo->name);
+ 			utimes(filepath, ftimes);
+ 		}
+ 	}
