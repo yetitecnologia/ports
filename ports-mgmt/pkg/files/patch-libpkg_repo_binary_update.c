c7a5b489ec8aee10391db1264596165b3aff4e03
Franco Fichtner <franco@opnsense.org>
2023-11-29 07:58:09 +0100

    ports-mgmt/pkg: version 2 of the patch
    PR: https://github.com/freebsd/pkg/issues/2200

--- ports-mgmt/pkg/files/patch-libpkg_repo_binary_update.c
+++ ports-mgmt/pkg/files/patch-libpkg_repo_binary_update.c
@@ -8,58 +8,79 @@
  #include <sys/time.h>
  
  #include <stdio.h>
-@@ -465,9 +466,9 @@ pkg_repo_binary_update_proceed(const char *name, struc
- 	struct pkg_manifest_key *keys = NULL;
- 	size_t len = 0;
- 	bool in_trans = false;
--	char *path = NULL;
-+	char *path = NULL, *lockpath = NULL;
- 	FILE *f = NULL;
--	int fd;
-+	int fd, ld = -1;
- 	char *line = NULL;
- 	size_t linecap = 0;
- 	ssize_t linelen, totallen = 0;
-@@ -490,6 +491,16 @@ pkg_repo_binary_update_proceed(const char *name, struc
- 		repo->meta->manifests, &local_t, &rc, &len);
- 	if (fd == -1)
- 		goto cleanup;
-+
-+	xasprintf(&lockpath, "%s-lock", name);
+@@ -563,12 +564,12 @@ cleanup:
+ 		if (pkgdb_transaction_commit_sqlite(sqlite, "REPO") != EPKG_OK)
+ 			rc = EPKG_FATAL;
+ 	}
+-	/* restore the previous db in case of failures */
+-	if (rc != EPKG_OK && rc != EPKG_UPTODATE) {
+-		unlink(name);
+-		rename(path, name);
+-	}
+ 	if (path != NULL) {
++		/* restore the previous db in case of failures */
++		if (rc != EPKG_OK && rc != EPKG_UPTODATE) {
++			unlink(name);
++			rename(path, name);
++		}
+ 		unlink(path);
+ 		free(path);
+ 	}
+@@ -586,13 +587,14 @@ int
+ pkg_repo_binary_update(struct pkg_repo *repo, bool force)
+ {
+ 	char filepath[MAXPATHLEN];
++	char *lockpath = NULL;
+ 	const char update_finish_sql[] = ""
+ 		"DROP TABLE repo_update;";
+ 	sqlite3 *sqlite;
+ 
+ 	struct stat st;
+ 	time_t t = 0;
+-	int res = EPKG_FATAL;
++	int ld, res = EPKG_FATAL;
+ 
+ 	bool got_meta = false;
+ 
+@@ -603,7 +605,7 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+ 
+ 	pkg_debug(1, "PkgRepo: verifying update for %s", pkg_repo_name(repo));
+ 
+-	/* First of all, try to open and init repo and check whether it is fine */
++	/* First of all, try to open repo and check whether it is fine */
+ 	if (repo->ops->open(repo, R_OK|W_OK) != EPKG_OK) {
+ 		pkg_debug(1, "PkgRepo: need forced update of %s", pkg_repo_name(repo));
+ 		t = 0;
+@@ -627,6 +629,15 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+ 		}
+ 	}
+ 
++	xasprintf(&lockpath, "%s-lock", filepath);
 +	ld = open(lockpath, O_CREAT|O_TRUNC|O_WRONLY, 00644);
 +	if (flock(ld, LOCK_EX|LOCK_NB) == -1) {
-+		pkg_emit_error("repository %s could not be locked for update",
++		pkg_emit_notice("Another process is updating repository %s",
 +			repo->name);
-+		rc = EPKG_FATAL;
++		res = EPKG_FATAL;
 +		goto cleanup;
 +	}
 +
- 	f = fdopen(fd, "r");
- 	rewind(f);
- 
-@@ -555,7 +566,16 @@ pkg_repo_binary_update_proceed(const char *name, struc
- 	 );
+ 	res = pkg_repo_binary_update_proceed(filepath, repo, &t, force);
+ 	if (res != EPKG_OK && res != EPKG_UPTODATE) {
+ 		pkg_emit_notice("Unable to update repository %s", repo->name);
+@@ -640,6 +651,16 @@ pkg_repo_binary_update(struct pkg_repo *repo, bool for
+ 	}
  
  cleanup:
 +	if (ld != -1) {
 +		flock(ld, LOCK_UN);
 +		close(ld);
 +	}
- 
++
 +	if (lockpath != NULL) {
 +		unlink(lockpath);
 +		free(lockpath);
 +	}
 +
- 	if (in_trans) {
- 		if (rc != EPKG_OK)
- 			pkgdb_transaction_rollback_sqlite(sqlite, "REPO");
-@@ -564,7 +584,7 @@ cleanup:
- 			rc = EPKG_FATAL;
- 	}
- 	/* restore the previous db in case of failures */
--	if (rc != EPKG_OK && rc != EPKG_UPTODATE) {
-+	if (rc != EPKG_OK && rc != EPKG_UPTODATE && path != NULL) {
- 		unlink(name);
- 		rename(path, name);
- 	}
+ 	/* Set mtime from http request if possible */
+ 	if (t != 0 && res == EPKG_OK) {
+ 		struct timeval ftimes[2] = {
